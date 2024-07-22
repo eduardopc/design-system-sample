a6eae63688ea8b0fc38c0c75c33092c35910b5cc
Eduardo Pacheco Celeste <eduardo.celeste@pipefy.com>
2024-07-22 14:10:58 -0300

    chore: add new packages

--- packages/tokens/src/colors.ts
+++ packages/tokens/src/colors.ts
@@ -11,8 +11,8 @@ export const colors = {
   gray800: '#202024',
   gray900: '#121214',
 
-  ignite300: '#00B37E',
-  ignite500: '#00875F',
-  ignite700: '#015F43',
-  ignite900: '#00291D',
+  base300: '#00B37E',
+  base500: '#00875F',
+  base700: '#015F43',
+  base900: '#00291D',
 }
